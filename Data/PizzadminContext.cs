3f6edbc1b86f56a52978f85a3ef287dda6f14efd
Edmir <User@DESKTOP-8BAG8JO>
2025-08-07 16:29:14 +0200

    Add project files.

---
+++ Data/PizzadminContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Pizzadmin.Data
+{
+    public class PizzadminContext : DbContext
+    {
+        public PizzadminContext(DbContextOptions<PizzadminContext> options) : base(options) { }
+    }
+}
