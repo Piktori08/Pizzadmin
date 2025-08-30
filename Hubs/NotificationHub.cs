3089ff234f607dcae0155d1b8371c5c8d2f7e4fc
Edmir <User@DESKTOP-8BAG8JO>
2025-08-30 14:57:35 +0200

    ndihmmm

---
+++ Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Pizzadmin.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task NewOrderPlaced(string orderId)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", $" A new order #{orderId} has been created!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,57 +1,66 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 using Microsoft.EntityFrameworkCore;
 using Pizzadmin.Data;
+using Pizzadmin.Hubs;
 using Pizzadmin.Identity;
 using Pizzadmin.Repositories;
 using Pizzadmin.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<PizzadminContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("PizzadminContext") ??
+
+builder.Services.AddDbContext<PizzadminContext>(Options => Options.UseSqlServer(
+    builder.Configuration.GetConnectionString("PizzadminContext") ??
     throw new InvalidOperationException("Connection string not found")));
 
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<PizzadminContext>().AddDefaultTokenProviders();
-
+// Identity
+builder.Services.AddIdentity<AppUser, AppRole>()
+    .AddEntityFrameworkStores<PizzadminContext>()
+    .AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddAuthorization();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
         policy => policy
-            .AllowAnyOrigin()
+            .WithOrigins(
+                "https://localhost:44365",
+                "http://localhost:3000",
+                "http://127.0.0.1:5500",    // Add this line
+                "http://localhost:5500",    // Add this too
+                "null"
+            )
             .AllowAnyHeader()
-            .AllowAnyMethod());
+            .AllowAnyMethod()
+            .AllowCredentials());
 });
+
 builder.Services.AddControllersWithViews()
-.AddNewtonsoftJson(options =>
- {
-     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-     options.SerializerSettings.MaxDepth = 32; // Adjust as needed
- });
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.MaxDepth = 32;
+    });
+
+// Add SignalR service
+builder.Services.AddSignalR();
 
 #region Repositories
-
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-
 builder.Services.AddScoped<ProductRepository, ProductRepository>();
 builder.Services.AddScoped<OrderRepository, OrderRepository>();
 builder.Services.AddScoped<OrderProductsRepository, OrderProductsRepository>();
-
 #endregion
 
-
 #region Services
-
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IRoleService, RoleService>();
-
 builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IOrderService, OrderService>();
 builder.Services.AddTransient<IOrderProductsService, OrderProductsService>();
 #endregion
-
 
 var app = builder.Build();
 
@@ -59,21 +68,22 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseCors("AllowAll"); // Moved before UseRouting
+
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseCors("AllowAll");
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}");
-    //pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<NotificationHub>("/notificationHub");
 
 app.Run();
